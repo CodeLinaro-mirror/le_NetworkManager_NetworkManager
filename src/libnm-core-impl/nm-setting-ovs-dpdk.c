095113410c844e6b844f5a0a4f858d11e1b00184
Ana Cabral <acabral@redhat.com>
2021-12-03 21:38:53 +0100

    openvswitch: Add n_rxq configuration option

--- src/libnm-core-impl/nm-setting-ovs-dpdk.c
+++ src/libnm-core-impl/nm-setting-ovs-dpdk.c
@@ -21,7 +21,10 @@
 
 /*****************************************************************************/
 
-NM_GOBJECT_PROPERTIES_DEFINE_BASE(PROP_DEVARGS, );
+NM_GOBJECT_PROPERTIES_DEFINE_BASE(PROP_DEVARGS, PROP_N_RXQ, );
+
+// PS.: Review changes suggested by Beniamino and Thomas are in a new commit
+// that will just be included here too, this is just for a test
 
 /**
  * NMSettingOvsDpdk:
@@ -32,6 +35,7 @@ struct _NMSettingOvsDpdk {
     NMSetting parent;
 
     char *devargs;
+    char *n_rxq;
 };
 
 struct _NMSettingOvsDpdkClass {
@@ -58,6 +62,24 @@ nm_setting_ovs_dpdk_get_devargs(NMSettingOvsDpdk *self)
     return self->devargs;
 }
 
+
+/**
+ * nm_setting_ovs_dpdk_get_n_rxq:
+ * @self: the #NMSettingOvsDpdk
+ *
+ * Returns: the #NMSettingOvsDpdk:n_rxq property of the setting
+ *
+ * Since: 1.35
+ **/
+const char *
+nm_setting_ovs_dpdk_get_n_rxq(NMSettingOvsDpdk *self)
+{
+    g_return_val_if_fail(NM_IS_SETTING_OVS_DPDK(self), NULL);
+
+    return self->n_rxq;
+}
+
+
 /*****************************************************************************/
 
 static void
@@ -69,6 +91,9 @@ get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
     case PROP_DEVARGS:
         g_value_set_string(value, self->devargs);
         break;
+    case PROP_N_RXQ:
+    	g_value_set_string(value, self->n_rxq);
+    	break;
     default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
         break;
@@ -79,12 +104,19 @@ static void
 set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
 {
     NMSettingOvsDpdk *self = NM_SETTING_OVS_DPDK(object);
+    gint int_val;
 
     switch (prop_id) {
     case PROP_DEVARGS:
         g_free(self->devargs);
         self->devargs = g_value_dup_string(value);
         break;
+    case PROP_N_RXQ:
+    	g_free(self->n_rxq);
+    	int_val = _nm_utils_ascii_str_to_int64(g_value_get_string(value), 10, 1, G_MAXINT32, 0);
+    	if(int_val)
+    		self->n_rxq = g_value_dup_string(value);
+    	break;
     default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
         break;
@@ -118,6 +150,7 @@ finalize(GObject *object)
     NMSettingOvsDpdk *self = NM_SETTING_OVS_DPDK(object);
 
     g_free(self->devargs);
+    g_free(self->n_rxq);
 
     G_OBJECT_CLASS(nm_setting_ovs_dpdk_parent_class)->finalize(object);
 }
@@ -145,6 +178,20 @@ nm_setting_ovs_dpdk_class_init(NMSettingOvsDpdkClass *klass)
         "",
         NULL,
         G_PARAM_READWRITE | NM_SETTING_PARAM_INFERRABLE | G_PARAM_STATIC_STRINGS);
+
+    /**
+	 * NMSettingOvsDpdk:n_rxq:
+	 *
+	 * Open vSwitch DPDK device arguments.
+	 *
+	 * Since: 1.35
+	 **/
+	obj_properties[PROP_N_RXQ] = g_param_spec_string(
+		NM_SETTING_OVS_DPDK_N_RXQ,
+		"",
+		"",
+		NULL,
+		G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
     g_object_class_install_properties(object_class, _PROPERTY_ENUMS_LAST, obj_properties);
 
--- src/libnm-core-public/nm-setting-ovs-dpdk.h
+++ src/libnm-core-public/nm-setting-ovs-dpdk.h
@@ -28,6 +28,7 @@ G_BEGIN_DECLS
 #define NM_SETTING_OVS_DPDK_SETTING_NAME "ovs-dpdk"
 
 #define NM_SETTING_OVS_DPDK_DEVARGS "devargs"
+#define NM_SETTING_OVS_DPDK_N_RXQ "n_rxq"
 
 typedef struct _NMSettingOvsDpdkClass NMSettingOvsDpdkClass;
 
@@ -37,7 +38,8 @@ NM_AVAILABLE_IN_1_20
 NMSetting *nm_setting_ovs_dpdk_new(void);
 
 NM_AVAILABLE_IN_1_20
-const char *nm_setting_ovs_dpdk_get_devargs(NMSettingOvsDpdk *self);
+const char * nm_setting_ovs_dpdk_get_devargs(NMSettingOvsDpdk *self);
+const char * nm_setting_ovs_dpdk_get_n_rxq(NMSettingOvsDpdk *self);
 
 G_END_DECLS
 
