55cf66392c2679c011ddfca490427bfcb4e0b22c
Thomas Haller <thaller@redhat.com>
2023-03-14 08:48:07 +0100

    libnm: make "connection.read-only" as deprecated
    This has no more meaning, and is always false.

--- src/libnm-core-public/nm-setting-connection.h
+++ src/libnm-core-public/nm-setting-connection.h
@@ -170,6 +170,8 @@ int nm_setting_connection_get_autoconnect_retries(NMSettingConnection *setting);
 NM_AVAILABLE_IN_1_14
 NMConnectionMultiConnect nm_setting_connection_get_multi_connect(NMSettingConnection *setting);
 guint64                  nm_setting_connection_get_timestamp(NMSettingConnection *setting);
+
+NM_DEPRECATED_IN_1_44
 gboolean                 nm_setting_connection_get_read_only(NMSettingConnection *setting);
 
 guint32     nm_setting_connection_get_num_permissions(NMSettingConnection *setting);
