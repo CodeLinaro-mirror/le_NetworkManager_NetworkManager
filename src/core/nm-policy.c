NetworkManager/NetworkManager

commit aede228974beed3764352e0e31096a70b8d2a49f

core: assert that devices are not registered when disposing NMPolicy
NMDevice holds a reference to NMManager, which holds a reference to NMPolicy.
It is not possible that we try to dispose NMPolicy while there are still devices
registered. That would be a bug, that we need to find and solve
differently. Add an assertion instead of trying to handle it.

src/core/nm-policy.c
@@ -2870,19 +2870,15 @@ dispose(GObject *object)
 {
     NMPolicy        *self = NM_POLICY(object);
     NMPolicyPrivate *priv = NM_POLICY_GET_PRIVATE(self);
-    GHashTableIter   h_iter;
-    NMDevice        *device;
+
+    nm_assert(!c_list_is_empty(&priv->policy_auto_activate_lst_head));
+    nm_assert(g_hash_table_size(priv->devices) == 0);
 
     nm_clear_g_object(&priv->default_ac4);
     nm_clear_g_object(&priv->default_ac6);
     nm_clear_g_object(&priv->activating_ac4);
     nm_clear_g_object(&priv->activating_ac6);
     nm_clear_pointer(&priv->pending_active_connections, g_hash_table_unref);
-
-    while ((device = c_list_first_entry(&priv->policy_auto_activate_lst_head,
-                                        NMDevice,
-                                        policy_auto_activate_lst)))
-        _auto_activate_device_clear(self, device, FALSE);
 
     g_slist_free_full(priv->pending_secondaries, (GDestroyNotify) pending_secondary_data_free);
     priv->pending_secondaries = NULL;
@@ -2900,12 +2896,6 @@ dispose(GObject *object)
     if (priv->dns_manager) {
         nm_clear_g_signal_handler(priv->dns_manager, &priv->config_changed_id);
         g_clear_object(&priv->dns_manager);
-    }
-
-    g_hash_table_iter_init(&h_iter, priv->devices);
-    while (g_hash_table_iter_next(&h_iter, (gpointer *) &device, NULL)) {
-        g_hash_table_iter_remove(&h_iter);
-        devices_list_unregister(self, device);
     }
 
     /* The manager should have disposed of ActiveConnections already, which

CodeLinaro-mirror/le_NetworkManager_NetworkManager

commit ba0835109c947fb4bf349143c9cb5f6c837b71b8

policy: assert that the auto-activate list is empty on dispose
We are currently asserting that the list of devices waiting for
auto-activation in NMPolicy is not empty. This condition is always
false because:

 - NMDevice holds a reference to NMManager
 - NMManager holds a reference to NMPolicy
 - on dispose, NMDevice asserts that it's not in NMPolicy's
   auto-activate list

Therefore if there is any NMDevice alive, NMPolicy must be alive as
well. Instead, if there is no NMDevice alive the list must be empty.

The assertion could fail only when the NMPolicy instance gets
disposed, which usually doesn't happen because it's still referenced
at shutdown.

Fixes: aede228974be ('core: assert that devices are not registered when disposing NMPolicy')

src/core/nm-policy.c
@@ -2934,7 +2934,7 @@ dispose(GObject *object)
     NMPolicy        *self = NM_POLICY(object);
     NMPolicyPrivate *priv = NM_POLICY_GET_PRIVATE(self);
 
-    nm_assert(!c_list_is_empty(&priv->policy_auto_activate_lst_head));
+    nm_assert(c_list_is_empty(&priv->policy_auto_activate_lst_head));
     nm_assert(g_hash_table_size(priv->devices) == 0);
 
     nm_clear_g_object(&priv->default_ac4);