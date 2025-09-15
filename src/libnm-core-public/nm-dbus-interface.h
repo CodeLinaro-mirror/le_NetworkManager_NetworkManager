783a92abb9c01655345f1526e61f3caddc73a66b
Íñigo Huguet <ihuguet@riseup.net>
2025-09-15 15:05:36 +0200

    core, libnm: add the "networking off" reason
    When we disable networking with `nmcli networking off` the reason that
    is logged is "sleeping". Explain instead that networking is disabled.
    
    Before:
      device (lo): state change: activated -> deactivating (reason 'sleeping' ...
    
    After:
      device (lo): state change: activated -> deactivating (reason 'networking-off' ...

--- src/libnm-core-public/nm-dbus-interface.h
+++ src/libnm-core-public/nm-dbus-interface.h
@@ -643,7 +643,7 @@ typedef enum {
  *   via settings plugin ('unmanaged-devices' for keyfile or 'NM_CONTROLLED=no' for ifcfg-rh).
  *   Since: 1.48
  * @NM_DEVICE_STATE_REASON_UNMANAGED_USER_UDEV: The device is unmanaged via udev rule. Since: 1.48
-
+ * @NM_DEVICE_STATE_REASON_NETWORKING_OFF: NetworkManager was disabled (networking off)
  *
  * Device state change reason codes
  */
@@ -726,6 +726,7 @@ typedef enum {
     NM_DEVICE_STATE_REASON_UNMANAGED_USER_EXPLICIT        = 75,
     NM_DEVICE_STATE_REASON_UNMANAGED_USER_SETTINGS        = 76,
     NM_DEVICE_STATE_REASON_UNMANAGED_USER_UDEV            = 77,
+    NM_DEVICE_STATE_REASON_NETWORKING_OFF                 = 78,
 } NMDeviceStateReason;
 
 /**
