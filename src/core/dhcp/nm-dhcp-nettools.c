NetworkManager/NetworkManager

commit 1db882ca63a577fd0d7adeb3af9f9bcaefe290bf

dhcp: allow the plugin to be restarted
In the next commit, a mechanism will be added to stop the DHCP plugin
and restart it without destroying the NMDhcpClient object. For this to
work, we must reset some members of the object when stopping or
starting the plugin.

src/core/dhcp/nm-dhcp-nettools.c
@@ -1323,7 +1323,7 @@ ip4_start(NMDhcpClient *client, GError **error)
     g_return_val_if_fail(!priv->probe, FALSE);
     g_return_val_if_fail(client_config, FALSE);
 
-    if (!nettools_create(self, &effective_client_id, error))
+    if (!priv->client && !nettools_create(self, &effective_client_id, error))
         return FALSE;
 
     r = n_dhcp4_client_probe_config_new(&config);

src/core/dhcp/nm-dhcp-client.c
@@ -1360,6 +1360,8 @@ nm_dhcp_client_start(NMDhcpClient *self, GError **error)
     g_return_val_if_fail(priv->config.uuid, FALSE);
     nm_assert(!priv->effective_client_id);
 
+    priv->is_stopped = FALSE;
+
     IS_IPv4 = NM_IS_IPv4(priv->config.addr_family);
 
     if (!IS_IPv4) {
@@ -1473,6 +1475,7 @@ nm_dhcp_client_stop(NMDhcpClient *self, gboolean release)
     if (priv->is_stopped)
         return;
 
+    nm_clear_pointer(&priv->effective_client_id, g_bytes_unref);
     nm_clear_g_source_inst(&priv->previous_lease_timeout_source);
 
     priv->is_stopped = TRUE;
@@ -1979,8 +1982,6 @@ dispose(GObject *object)
         nm_clear_g_source_inst(&priv->v6.lladdr_timeout_source);
         nm_clear_g_source_inst(&priv->v6.dad_timeout_source);
     }
-
-    nm_clear_pointer(&priv->effective_client_id, g_bytes_unref);
 
     nm_assert(!priv->watch_source);
     nm_assert(!priv->l3cd_next);

CodeLinaro-mirror/le_NetworkManager_NetworkManager

commit 348af3aa3005ee0b43f9bfed33a14c00c990a035

dhcp/nettools: don't clear effective client-id on restart
When ip4_start() is called after a restart (priv->client already exists),
nettools_create() is skipped so effective_client_id stays NULL. The
unconditional nm_dhcp_client_set_effective_client_id() call then clears
the client-id that was properly set during the initial start.

Only update the effective client-id when it was actually obtained from
nettools_create().

Fixes: 1db882ca63a5 ('dhcp: allow the plugin to be restarted')

src/core/dhcp/nm-dhcp-nettools.c
@@ -1525,7 +1525,8 @@ ip4_start(NMDhcpClient *client, GError **error)
 
     _LOGT("dhcp-client4: start " NM_HASH_OBFUSCATE_PTR_FMT, NM_HASH_OBFUSCATE_PTR(priv->client));
 
-    nm_dhcp_client_set_effective_client_id(client, effective_client_id);
+    if (effective_client_id)
+        nm_dhcp_client_set_effective_client_id(client, effective_client_id);
 
     return TRUE;
 }